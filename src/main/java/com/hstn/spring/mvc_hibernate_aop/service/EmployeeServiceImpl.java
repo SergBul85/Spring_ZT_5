2c3e9e45cf5cb57ee72a17b710cc405d8ba2d952
SergBul85 <donetsk55555@gmail.com>
2026-05-16 14:54:53 +0300

    L_71_an_service

---
+++ src/main/java/com/hstn/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.hstn.spring.mvc_hibernate_aop.service;
+
+import com.hstn.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.hstn.spring.mvc_hibernate_aop.dao.EmployeeDAOImpl;
+import com.hstn.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+}
